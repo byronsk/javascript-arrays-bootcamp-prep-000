27c8702e7ad8a33a6a52b3b9e0ea65c5a57aa245
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-22 12:53:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {
   chocolateBars.shift();
   return [chocolateBars];
 }
+
+function removeElementFromBeginningOfArray (array) {
+  
+}
