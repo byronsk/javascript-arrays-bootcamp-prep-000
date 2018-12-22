eb251c59fd027facdc13ac60b3d9750d3fcf0f0b
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-22 12:35:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray (chocolateBars, element) {
 return [element, ...chocolateBars];
 }
+
+function destructivelyAddElementToBeginningOfArray (chocolateBars, element) {
+  return [chocolateBars]
+}
